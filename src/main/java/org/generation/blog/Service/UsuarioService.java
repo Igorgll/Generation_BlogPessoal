32a68943237157c6b130aa89f1b03d740585b53d
Igor Gonçalves Lima <62486132+Igorgll@users.noreply.github.com>
2022-04-06 12:42:24 -0300

    Update UsuarioService.java

--- src/main/java/org/generation/blog/Service/UsuarioService.java
+++ src/main/java/org/generation/blog/Service/UsuarioService.java
@@ -17,71 +17,49 @@ import org.springframework.web.server.ResponseStatusException;
 public class UsuarioService {
 
     @Autowired
-	private UsuarioRepository repository;
-	
-	public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
-		Optional<Usuario> optional = repository.findByUsuario(usuario.getUsuario());
-		if (optional.isPresent()) {
-			return Optional.empty();
-		}
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		
-		String  senhaEncoder = encoder.encode(usuario.getSenha());
-		usuario.setSenha(senhaEncoder);
-		
-		return Optional.ofNullable(repository.save(usuario));
-	}
-	
+    private UsuarioRepository repository;
 
-	public Optional<UserLogin> Logar(Optional<UserLogin> user){
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		Optional <Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+    public Optional<Usuario> CadastrarUsuario(Usuario usuario) {
 
-		if(usuario.isPresent())	{
-			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())){
-				String auth = user.get().getUsuario() + ":" +user.get().getSenha();
-				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
-				String authHeader ="Basic " + new String(encodedAuth);
-				
-				user.get().setToken(authHeader);
-				user.get().setId(usuario.get().getId());
-				user.get().setNome(usuario.get().getNome());
-				user.get().setSenha(usuario.get().getSenha());
-				user.get().setUsuario(usuario.get().getUsuario());
-				user.get().setFoto(usuario.get().getFoto());
-				user.get().setTipo(usuario.get().getTipo());
-				
-				return user;
-			}
-		}
-        return Optional.empty();	
-	}
-	
-	
-	public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+        Optional<Usuario> novoUsuario = repository.findByUsuario(usuario.getUsuario());
 
-		if (repository.findById(usuario.getId()).isPresent()) {
-			Optional<Usuario> buscaUsuario = repository.findByUsuario(usuario.getUsuario());
+        if (novoUsuario.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
+        } else {
 
-			if (buscaUsuario.isPresent()) {				
-				if (buscaUsuario.get().getId() != usuario.getId())
-					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
-			}
-			
-			usuario.setSenha(criptografarSenha(usuario.getSenha()));
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-			return Optional.of(repository.save(usuario));
-		} 
-			
-		return Optional.empty();		
-	}	
+            String passwordEncoder = encoder.encode(usuario.getSenha());
+            usuario.setSenha(passwordEncoder);
 
-	private String criptografarSenha(String senha) {
+            return Optional.ofNullable(repository.save(usuario));
+        }
+    }
 
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		String senhaEncoder = encoder.encode(senha);
+    public Optional<UserLogin> Logar(Optional<UserLogin> user) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
 
-		return senhaEncoder;
-	}
-	
+        if (usuario.isPresent()) {
+            if (encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+
+                String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+                byte[] encoderAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+                String authHeader = "Basic " + new String(encoderAuth);
+                
+                user.get().setToken(authHeader);
+                user.get().setNome(usuario.get().getNome());
+                user.get().setUsuario(usuario.get().getUsuario());
+                user.get().setSenha(usuario.get().getSenha());
+                user.get().setFoto(usuario.get().getFoto());
+                user.get().setTipo(usuario.get().getTipo());
+                user.get().setId(usuario.get().getId());
+
+                return user;
+            }
+        }
+
+        return null;
+
+    }
 }
